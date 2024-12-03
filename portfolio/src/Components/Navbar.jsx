b50dc5364a3fa8ca11d2553e97953d7699ff8478
PrabhatYadav-27 <prabhaty2727@gmail.com>
2024-12-03 19:41:48 +0530

    added the menu and close button function using state

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import pic from '/src/assets/photo.avif'
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoCloseSharp } from "react-icons/io5";
 
 function NavBar() {
+    //managing the state for menu
+    const [menu, setMenu] = useState(false);
   return (
     <div className='max-w-screen-2xl container mx-auto pd-4 md:px-20 shadow-md'>
 
@@ -12,18 +16,28 @@ function NavBar() {
                 <p className='text-sm'>Software Developer</p>
                 </h1>
             </div>
+            {/* desktop navbar */}
             <div>
-                <ul className='flex space-x-8'>
+                <ul className=' hidden md:flex space-x-8'>
                     <li>Home</li>
                     <li>About</li>
                     <li>Portfolio </li>
                     <li>Experience</li>
                     <li>Contact</li>
                 </ul>
+                <div onClick={()=>setMenu(!menu)}>{menu ? <AiOutlineMenu />: <IoCloseSharp />}</div>
             </div>
         </div>
-
-        <div></div>
+    {/* mobile navbar */}
+        <div>
+            <ul className='md:hidden'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Portfolio </li>
+                <li>Experience</li>
+                <li>Contact</li>
+            </ul>
+        </div>
     </div>
   )
 }
