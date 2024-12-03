96d744c2334b5fc0db79682f71195a62c8a4b8a9
PrabhatYadav-27 <prabhaty2727@gmail.com>
2024-12-03 20:06:33 +0530

    done with navbar

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -6,8 +6,31 @@ import { IoCloseSharp } from "react-icons/io5";
 function NavBar() {
     //managing the state for menu
     const [menu, setMenu] = useState(false);
+    //making array to map the pages home,contact,etc
+    const navItems= [
+        {
+            id:1,
+            text:"Home"
+        },
+        {
+            id:2,
+            text:"About"
+        },
+        {
+            id:3,
+            text:"Portfolio"
+        },
+        {
+            id:4,
+            text:"Experience"
+        },
+        {
+            id:5,
+            text:"Contact"
+        },
+    ]
   return (
-    <div className='max-w-screen-2xl container mx-auto pd-4 md:px-20 shadow-md'>
+    <div className='max-w-screen-2xl container mx-auto pd-4 md:px-20 shadow-md h-16 fixed top-0 left-0 right-0'>
 
         <div className='flex justify-between items-center h-16'>
             <div className='flex space-x-2'>
@@ -19,25 +42,32 @@ function NavBar() {
             {/* desktop navbar */}
             <div>
                 <ul className=' hidden md:flex space-x-8'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Portfolio </li>
-                    <li>Experience</li>
-                    <li>Contact</li>
+                    {
+                        navItems.map(({id,text})=>(
+                            <li className='hover:scale-110 duration-200 cursor-pointer' key={id}>{text}</li>
+                        ))
+                    }
                 </ul>
-                <div onClick={()=>setMenu(!menu)}>{menu ? <AiOutlineMenu />: <IoCloseSharp />}</div>
+                <div onClick={()=>setMenu(!menu)} className='md:hidden'>{menu ? <AiOutlineMenu size={24}/>: <IoCloseSharp size={24}/>}</div>
             </div>
         </div>
     {/* mobile navbar */}
-        <div>
-            <ul className='md:hidden'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Portfolio </li>
-                <li>Experience</li>
-                <li>Contact</li>
+
+    {
+        menu && (
+            <div>
+            <ul className='md:hidden flex flex-col h-screen items-center justify-center space-y-3 text-xl'>
+            {
+                 navItems.map(({id,text})=>(
+                    <li className='hover:scale-110 duration-200  font-semibold cursor-pointer' key={id}>{text}</li>
+                ))
+            }
             </ul>
         </div>
+
+        )
+    }
+        
     </div>
   )
 }
